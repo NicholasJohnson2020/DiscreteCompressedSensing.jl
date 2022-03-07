3de96dd61266a6236bb9d081a656701d5b9b9c0c
Nicholas Johnson <>
2022-03-07 00:17:01 -0500

    more dev work

--- discreteCompressedSensing.jl
+++ discreteCompressedSensing.jl
@@ -1,5 +1,6 @@
-using JuMP, SCS, Gurobi, LinearAlgebra
+using JuMP, SCS, Gurobi, LinearAlgebra, Random
 
 include("src/basisPursuitDenoising.jl")
 include("src/exactCompressedSensing.jl")
 include("src/convexRelaxation.jl")
+include("src/cuttingPlanes.jl")
