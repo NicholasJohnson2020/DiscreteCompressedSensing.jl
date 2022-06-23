a82b2f26a058904a7aa676399d1bbafd0612ca08
Nicholas Johnson <>
2022-06-23 19:50:29 -0400

    patched SOC function

--- src/convexRelaxation.jl
+++ src/convexRelaxation.jl
@@ -16,11 +16,11 @@ function perspectiveRelaxation(A, b, epsilon, lambda;
     @variable(model, x[i=1:n])
     @variable(model, z[i=1:n] >= 0)
     @variable(model, theta[i=1:n] >= 0)
-    #@variable(model, residual[i=1:m])
+    @variable(model, residual[i=1:m])
 
-    #@constraint(model, residual .== A * x .- b)
-    #@constraint(model, sum(residual[i]^2 for i=1:m) <= epsilon)
-    @constraint(model, [epsilon^0.5; A*x.-b] in SecondOrderCone())
+    @constraint(model, residual .== A * x .- b)
+    @constraint(model, sum(residual[i]^2 for i=1:m) <= epsilon)
+    #@constraint(model, [epsilon^0.5; A*x.-b] in SecondOrderCone())
 
     @constraint(model, [i=1:n], z[i] * theta[i] >= x[i]^2)
 
