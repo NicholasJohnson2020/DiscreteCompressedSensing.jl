3f2d80e69c626473f2a369b1c3b32d3ee62a7f40
Nicholas Johnson <>
2022-10-31 23:16:15 -0400

    code updates

--- discreteCompressedSensing.jl
+++ discreteCompressedSensing.jl
@@ -1,7 +1,11 @@
-using JuMP, SCS, Gurobi, LinearAlgebra, Random, Dates
+using JuMP, SCS, Gurobi, LinearAlgebra, Random, Dates, SparseArrays, OSQP
 
 include("src/basisPursuitDenoising.jl")
 include("src/exactCompressedSensing.jl")
 include("src/convexRelaxation.jl")
 include("src/cuttingPlanes.jl")
 include("src/cuttingPlanesOpt.jl")
+include("src/cuttingPlanesOptV2.jl")
+
+include("src/CSBnB.jl")
+include("src/KSVD.jl")
