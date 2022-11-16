c7d2a4f97c4dd66823620b9444a150a873742572
Nicholas Johnson <>
2022-11-16 01:28:16 -0500

    param gen edits

--- scripts/generateECGParameters.jl
+++ scripts/generateECGParameters.jl
@@ -5,8 +5,9 @@ using JSON
 
 file_path = ARGS[1]
 
-NUM_CR = collect(1:2:19)
+#NUM_CR = collect(1:2:19)
 #EPSILON_MULTIPLE = collect(0.05:0.05:0.5)
+M_vals = collect(5:5:50)
 EPSILON_MULTIPLE = collect(1:0.05:1.5)
 N = 1024
 
@@ -14,10 +15,11 @@ config_count = 0
 
 param_dict = Dict()
 
-for num in NUM_CR, eps in EPSILON_MULTIPLE
+#for num in NUM_CR, eps in EPSILON_MULTIPLE
+for M in M_vals, eps in EPSILON_MULTIPLE
 
     global config_count += 1
-    M = Int64(floor((1-num*5*0.01)*N))
+    #M = Int64(floor((1-num*5*0.01)*N))
     CR = 1 - M/N
     param_dict[config_count] = Dict("M"=>M,
                                     "CR"=>CR,
