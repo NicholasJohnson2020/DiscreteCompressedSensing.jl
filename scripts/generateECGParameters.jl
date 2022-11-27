0060e2fd9c626e0946316bdd5ee9cee28dac93ce
Nicholas Johnson <>
2022-11-27 02:48:48 -0500

    param gen edit

--- scripts/generateECGParameters.jl
+++ scripts/generateECGParameters.jl
@@ -10,21 +10,29 @@ file_path = ARGS[1]
 M_vals = collect(10:5:45)
 EPSILON_MULTIPLE = collect(1:0.05:1.5)
 N = 1024
+GAMMA_MULT = [0.01, 0.1, 1, 10]
+GAMMA_FLAG = ["SQUARE_ROOT", "LINEAR", "SQUARE"]
 
 config_count = 0
 
 param_dict = Dict()
 
 #for num in NUM_CR, eps in EPSILON_MULTIPLE
-for M in M_vals, eps in EPSILON_MULTIPLE
+for gamma_flag in GAMMA_FLAG
+    for gamma_mult in GAMMA_MULT
+        for M in M_vals, eps in EPSILON_MULTIPLE
 
-    global config_count += 1
-    #M = Int64(floor((1-num*5*0.01)*N))
-    CR = 1 - M/N
-    param_dict[config_count] = Dict("M"=>M,
-                                    "CR"=>CR,
-                                    "EPSILON"=>eps)
+            global config_count += 1
+            #M = Int64(floor((1-num*5*0.01)*N))
+            CR = 1 - M/N
+            param_dict[config_count] = Dict("M"=>M,
+                                            "CR"=>CR,
+                                            "EPSILON"=>eps,
+                                            "GAMMA_MULT"=>gamma_mult,
+                                            "GAMMA_FLAG"=>gamma_flag)
 
+        end
+    end
 end
 
 f = open(file_path * "params.json","w")
