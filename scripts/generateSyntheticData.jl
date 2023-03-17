9b08d1fb426077c9ecc89a10bfbd65c14d75a077
Nicholas Johnson <>
2023-03-17 13:30:40 -0400

    updated data generation script

--- scripts/generateSyntheticData.jl
+++ scripts/generateSyntheticData.jl
@@ -1,14 +1,11 @@
-using Pkg
-Pkg.activate("/home/nagj/.julia/environments/sparse_discrete")
-
 using JSON, Random, Distributions
 
-function sample_data(n, p, k, sigma, noise_ratio)
+function sample_data(m, n, k, sigma, signal_to_noise_ratio)
 
-    data_dist = Normal(0, sigma)
-    X = rand(data_dist, (n, p))
+    data_dist = Normal(0, signal_to_noise_ratio * sigma / n^0.5)
+    X = rand(data_dist, (m, n))
 
-    beta_temp = rand(data_dist, p)
+    beta_temp = rand(data_dist, n)
     indices = randperm(p)[1:k]
     beta = zeros(p)
     for index in indices
@@ -16,8 +13,8 @@ function sample_data(n, p, k, sigma, noise_ratio)
     end
 
     Y = X * beta
-    noise_dist = Normal(0, noise_ratio * sigma)
-    noise = rand(noise_dist, n)
+    noise_dist = Normal(0, sigma)
+    noise = rand(noise_dist, m)
     Y = Y + noise
 
     return X, Y, k, beta
@@ -26,12 +23,11 @@ end
 
 file_path = ARGS[1]
 
-NUM_TRIALS_PER_CONFIG = 5
+NUM_TRIALS_PER_CONFIG = 200
 N = collect(100:100:1000)
-P = collect(20:10:80)
-K = collect(5:5:15)
-#noise_to_signal = collect(0.8:0.1:1.2)
-noise_to_signal = 0.8
+M = [100]
+K = [10]
+alpha = [0.5]
 
 sigma = 1
 
@@ -42,20 +38,20 @@ param_dict = Dict()
 
 data_dict["Trials"] = NUM_TRIALS_PER_CONFIG
 data_dict["N"] = N
-data_dict["P"] = P
+data_dict["P"] = M
 data_dict["K"] = K
-data_dict["noise_to_signal"] = noise_to_signal
+data_dict["signal_to_noise"] = alpha
 data_dict["sigma"] = sigma
 
-for n in N, p in P, k in K, noise_ratio in noise_to_signal
+for n in N, m in M, k in K, signal_to_noise in alpha
 
     global config_count += 1
-    param_dict[config_count] = Dict("N"=>n, "P"=>p, "K"=>k,
-                                    "noise_ratio"=>noise_ratio)
+    param_dict[config_count] = Dict("N"=>n, "M"=>m, "K"=>k,
+                                    "signal_ratio"=>signal_to_noise)
 
     current_data_dict = Dict()
     for trial_num = 1:NUM_TRIALS_PER_CONFIG
-        X, Y, _, beta = sample_data(n, p, k, sigma, noise_ratio)
+        X, Y, _, beta = sample_data(m, n, k, sigma, signal_to_noise)
         current_data_dict[trial_num] = Dict("X"=>X, "Y"=>Y, "k"=>k,
                                             "beta"=>beta)
     end
