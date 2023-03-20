105c33be898b0bc1f40cec1992b57743a820b363
Nicholas Johnson <>
2023-03-20 01:53:48 -0400

    bug patch

--- discreteCompressedSensing.jl
+++ discreteCompressedSensing.jl
@@ -1,7 +1,8 @@
 using JuMP, SCS, Gurobi, LinearAlgebra, Random, Dates, SparseArrays
 using DataStructures, ProgressMeter, Base.Threads
 using JSON, MAT, Statistics, Distributions
-using TSSOS, DynamicPolynomials, SumOfSquares, Mosek
+using DynamicPolynomials, SumOfSquares, Mosek
+#using TSSOS
 
 GUROBI_ENV = Gurobi.Env()
 
