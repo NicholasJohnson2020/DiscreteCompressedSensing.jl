74b24d2e73f10b879c9626f6a56019b4e0110bf0
Nicholas Johnson <>
2023-04-21 17:34:51 -0400

    small fix

--- scripts/generateSyntheticData.jl
+++ scripts/generateSyntheticData.jl
@@ -24,12 +24,11 @@ end
 file_path = ARGS[1]
 
 NUM_TRIALS_PER_CONFIG = 100
-N = collect(100:100:800)
-#N = [200]
+#N = collect(100:100:800)
+N = [200]
 #M = collect(100:100:800)
 M = [100]
-#K = collect(10:5:55)
-K = [10]
+K = collect(10:5:55)
 #ratios = collect(10:2:28)
 ratios = [10]
 #alphas = collect(0.05:0.05:0.9)
