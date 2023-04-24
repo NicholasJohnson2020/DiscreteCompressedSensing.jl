dc6d849475fe4c16b5cffb2f8be691f606c4f681
Nicholas Johnson <>
2023-04-24 05:15:36 -0400

    small fix

--- scripts/executeECGExperiments.jl
+++ scripts/executeECGExperiments.jl
@@ -64,6 +64,7 @@ atom_dict, _ = ksvd(
 task_ID_list = collect((task_ID_input+1):num_tasks_input:length(param_dict))
 if method_name in ["OMP", "IRWL1_Rounded", "BPD_Rounded"]
     task_ID_list = [1]
+end
 
 for TASK_ID in task_ID_list
 
