263e9abe10cbd2e703ea31192c6f5468400b7b5a
Nicholas Johnson <>
2024-01-30 14:44:22 -0500

    added ML experiment consolidation script

---
+++ scripts/consolidateMLData.jl
@@ -0,0 +1,81 @@
+using JSON, LinearAlgebra, Statistics, DataFrames, CSV
+
+function processData(input_path, prefix; BnB=false)
+   """
+   This function loads raw experiment output data and processes it into a
+   dataframe.
+   """
+   df = DataFrame(index=Int64[], epsilon_multiple=Float64[],
+                  gamma_mult=Float64[], gamma_flag=String[], L2_error=Float64[],
+                  L2_error_rel=Float64[], L0_norm=Int64[], TPR=Float64[],
+                  TNR=Float64[], acc=Float64[], exec_time=Float64[])
+
+   successful_entries = 0
+
+   file_paths = readdir(input_path, join=true)
+   num_nodes = []
+
+   # Iterate over all files in the input directory
+   for file_name in file_paths
+
+      exp_data = Dict()
+      open(file_name, "r") do f
+         dicttxt = JSON.read(f, String)
+         exp_data = JSON.parse(dicttxt)
+         exp_data = JSON.parse(exp_data)
+      end
+
+      # Extract and store the relevant data
+      current_row = [exp_data["TASK_ID"],
+                     exp_data["EPSILON"],
+                     exp_data["GAMMA_MULT"],
+                     exp_data["GAMMA_FLAG"],
+                     exp_data[prefix * "L2_error"],
+                     exp_data[prefix * "L2_error_rel"],
+                     exp_data[prefix * "L0_norm"],
+                     exp_data[prefix * "TPR"],
+                     exp_data[prefix * "TNR"],
+                     exp_data[prefix * "accuracy"],
+                     exp_data[prefix * "execution_time"]]
+
+      if BnB
+         append!(num_nodes, exp_data["num_nodes"])
+      end
+      push!(df, current_row)
+      successful_entries += 1
+
+   end
+
+   if BnB
+      df[!, "num_nodes"] = num_nodes_mean
+   end
+
+   println("$successful_entries entries have been entered into the dataframe.")
+   return df
+end;
+
+METHOD_NAME = ARGS[1]
+INPUT_PATH = ARGS[2] * METHOD_NAME * "/"
+numerical_threshold = 1e-4
+
+# Process and save the data
+if METHOD_NAME in ["BnB_Primal", "BnB_Primal_2"]
+   df1 = processData(INPUT_PATH, "", BnB = true)
+else
+   df1 = processData(INPUT_PATH, "")
+end
+
+if METHOD_NAME in ["BPD_Rounded", "IRWL1_Rounded"]
+   df2 = processData(INPUT_PATH, "rounded_")
+   CSV.write(INPUT_PATH * METHOD_NAME * "_rounded_aggrData.csv", df2)
+end
+
+#if METHOD_NAME == "SOC_Relax_Rounded"
+#   df2 = processData(INPUT_PATH, "rounded_x_")
+#   df3 = processData(INPUT_PATH, "rounded_z_")
+#   CSV.write(INPUT_PATH * METHOD_NAME * "_rounded_x_aggrData.csv", df2)
+#   CSV.write(INPUT_PATH * METHOD_NAME * "_rounded_z_aggrData.csv", df3)
+#end
+
+CSV.write(INPUT_PATH * METHOD_NAME * "_aggrData.csv", df1)
+;
